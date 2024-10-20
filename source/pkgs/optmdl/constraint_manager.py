6c8c4c834176f223d678c5d0fda0321407414898
ryoTakayanagi <r-takayanag_i@outlook.com>
2024-10-20 13:48:07 +0900

    [FIX] コメント追加

--- source/pkgs/optmdl/constraint_manager.py
+++ source/pkgs/optmdl/constraint_manager.py
@@ -3,6 +3,17 @@ import pulp
 from pkgs.optmdl.sample_model import SampleModel
 
 def add_homeroom_constraints(model: SampleModel) -> SampleModel:
+    """
+    学級制約を追加する
+
+    任意の学級の任意の曜日・時限に対して，その学級のカリキュラムに含まれる講座の開講変数xの総和は1以上である。
+
+    Arguments:
+        model (SampleModel) -- モデル
+
+    Returns:
+        SampleModel -- 制約を追加したモデル
+    """
     for h in model.dto.homeroom_list:
         for d in model.dto.day_of_week:
             for p in model.dto.schedule[h][d]:
@@ -12,6 +23,17 @@ def add_homeroom_constraints(model: SampleModel) -> SampleModel:
 def add_course_credit_constraints(
     model: SampleModel, course_credit_dict: dict
 ) -> SampleModel:
+    """
+    単位数制約を追加する
+
+    任意の学級の任意の講座に対して，講座の開講変数xの曜日・時限に関する総和は，科目の単位数に等しい。
+
+    Arguments:
+        model (SampleModel) -- モデル
+
+    Returns:
+        SampleModel -- 制約を追加したモデル
+    """
     for h in model.dto.homeroom_list:
         for block in model.dto.curriculum_dict[h]:
             for lane in block:
@@ -24,6 +46,17 @@ def add_course_credit_constraints(
 
 
 def add_block_constraints(model: SampleModel) -> SampleModel:
+    """
+    ブロック制約を追加する
+
+    任意の学級について，任意の曜日・時限で，任意のブロックに対して，レーンに含まれる講座の開講変数xの総和はすべて等しい。
+
+    Arguments:
+        model (SampleModel) -- モデル
+
+    Returns:
+        SampleModel -- 制約を追加したモデル
+    """
     for h in model.dto.homeroom_list:
         for d in model.dto.day_of_week:
             for p in model.dto.schedule[h][d]:
@@ -35,7 +68,17 @@ def add_block_constraints(model: SampleModel) -> SampleModel:
     return model
 
 def add_teacher_constraints(model: SampleModel) -> SampleModel:
-    # 1教員が同日同時限に行える授業は1つまで
+    """
+    教員制約を追加する
+
+    任意の曜日・時限で，任意の教員に対して，教員の授業実施変数yは1以下である。
+
+    Arguments:
+        model (SampleModel) -- モデル
+
+    Returns:
+        SampleModel -- 制約を追加したモデル
+    """
     for d in model.dto.day_of_week:
         for p in [i for i in range(1,8)]:
             for t in model.dto.teacher_list:
