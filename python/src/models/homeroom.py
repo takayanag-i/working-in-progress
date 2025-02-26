4951c9c5154a647b30614957610485c261f36de1
ryoTakayanagi <r-takayanag_i@outlook.com>
2025-02-26 22:31:05 +0900

    [ADD] homeroom

--- python/src/models/homeroom.py
+++ python/src/models/homeroom.py
@@ -1,2 +1,19 @@
-class Homeroom:
-    pass
+from pydantic import BaseModel
+from typing import List
+
+
+class Schedule(BaseModel):
+    day: str
+    lastPeriod: int
+
+
+class Homeroom(BaseModel):
+    name: str
+    schedule: List[Schedule]
+
+
+class HomeroomSchema(BaseModel):
+    id: str
+    docType: str
+    ttid: str
+    homerooms: List[Homeroom]
--- python/tests/mock/homeroom.py
+++ python/tests/mock/homeroom.py
@@ -1,6 +1,12 @@
 from cruds.homeroom.interface import HomeroomRepository
+from models.homeroom import HomeroomSchema
+import json
+import os
 
 
 class HomeroomRepositoryMock(HomeroomRepository):
-    def find_by_ttid(self, ttid: str):
-        return
+    def find_by_ttid(self, ttid: str) -> HomeroomSchema:
+        file_path = os.path.join(os.path.dirname(__file__), '../resources/sample01/homeroom_schema.json')
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return HomeroomSchema(**data)
