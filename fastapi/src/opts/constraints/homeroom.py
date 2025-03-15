687886144a26969df7c59105798ba935a80ff45e
ryoTakayanagi <r-takayanag_i@outlook.com>
2025-03-15 11:08:15 +0900

    credit test

--- fastapi/src/opts/constraints/homeroom.py
+++ fastapi/src/opts/constraints/homeroom.py
@@ -5,10 +5,16 @@ import pulp
 
 class HomeroomConstraint(ConstraintBase):
     def apply(self, model: AnualModel) -> AnualModel:
-        for h in model.data.H:
-            for d in model.data.D:
-                for p in model.data.periods[h][d]:
-                    model.problem += pulp.lpSum(
-                        [model.x[h, d, p, c] for block in model.data.curriculum_dict[h] for lane in block for c in lane]
-                    ) >= 1
+        constraints = [
+            pulp.lpSum(
+                [model.x[h, d, p, c] for block in model.data.curriculums[h] for lane in block for c in lane]
+            ) >= 1
+            for h in model.data.H
+            for d in model.data.D
+            for p in model.data.periods[h][d]
+        ]
+
+        for constraint in constraints:
+            model.problem += constraint
+
         return model
--- fastapi/tests/unit/opts/constraints/test_credits.py
+++ fastapi/tests/unit/opts/constraints/test_credits.py
@@ -1,3 +1,4 @@
+import pulp
 from opts.constraints.credit import CreditConstraint
 
 
@@ -5,10 +6,53 @@ def test_credit_constraint(mock_anual_model):
     credit_constraint = CreditConstraint()
     model = credit_constraint.apply(mock_anual_model)
 
-    actual_constraints = [
-        (d["name"], d["value"], v.toDict()["constant"])
-        for v in model.problem.constraints.values()
-        for d in v.toDict()["coefficients"]
+    expected_constraints = [
+        {
+            "sence": pulp.LpConstraintEQ,
+            "coefficients": [
+                {"name": "x_H1_mon_1_C1", "value": 1},
+                {"name": "x_H1_mon_1_C2", "value": -1},
+                {"name": "x_H1_mon_1_C3", "value": -1},
+            ],
+            "constant": 1,
+        },
+        {
+            "sence": pulp.LpConstraintEQ,
+            "coefficients": [
+                {"name": "x_H1_mon_2_C1", "value": 1},
+                {"name": "x_H1_mon_2_C2", "value": -1},
+                {"name": "x_H1_mon_2_C3", "value": -1},
+            ],
+            "constant": 1,
+        },
+        {
+            "sence": pulp.LpConstraintEQ,
+            "coefficients": [
+                {"name": "x_H1_tue_1_C1", "value": 1},
+                {"name": "x_H1_tue_1_C2", "value": -1},
+                {"name": "x_H1_tue_1_C3", "value": -1},
+            ],
+            "constant": 1,
+        },
+        {
+            "sence": pulp.LpConstraintEQ,
+            "coefficients": [
+                {"name": "x_H1_tue_2_C1", "value": 1},
+                {"name": "x_H1_tue_2_C2", "value": -1},
+                {"name": "x_H1_tue_2_C3", "value": -1},
+            ],
+            "constant": 1,
+        },
     ]
 
+    # actual_constraints = [
+    #     (d["name"], d["value"], v.toDict()["constant"])
+    #     for v in model.problem.constraints.values()
+    #     for d in v.toDict()["coefficients"]
+    # ]
+
+    actual_constraints = [v.toDict() for v in model.problem.constraints.values()]
+
     assert len(actual_constraints) == 24, "数が合わない"
+    for e, a in zip(expected_constraints, actual_constraints):
+        assert e["sence"] == a["sence"], "senceが合わない"
