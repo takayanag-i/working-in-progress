c4eeef0ac12a48ba6af793887c9aa0964b371652
ryoTakayanagi <r-takayanag_i@outlook.com>
2025-03-19 22:28:02 +0900

    エンドポイント

--- fastapi/src/routers/timetable.py
+++ fastapi/src/routers/timetable.py
@@ -8,6 +8,11 @@ def home():
     return {"message": "ホームです"}
 
 
+@router.post("/api/instructors")
+def insert_instructors():
+    return {"message": "教員を追加しました"}
+
+
 @router.post("/api/solutions")
 def solve():
     return {"message": "解けました"}
